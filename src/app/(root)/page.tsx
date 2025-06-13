280f3b76019a8dc1378f97da474f61c470d0cfa8
김태우 <113758615+cher1shRXD@users.noreply.github.com>
2025-06-13 22:12:19 +0900

    Initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import CustomLink from '@/components/common/CustomLink'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <CustomLink href='/hi' className=''>welcome to cher1sh-next-app</CustomLink>
+    </div>
+  )
+}
+export default Home
