b833e30c95cacbf26e44cbc1834f4cc99cab0bfc
김태우 <taewoo080401@gmail.com>
2025-06-14 22:29:05 +0900

    feat: swap club button, logo position

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,9 +2,8 @@
 
 const Header = () => {
   return (
-    <div className="w-full h-12 flex justify-between items-center">
-      <p></p>
-      <p className="text-3xl font-extrabold font-anton tracking-[2.4] text-primary">CLUBBY</p>
+    <div className="w-full h-12 flex justify-center items-center">
+      <p className="text-3xl font-extrabold font-anton tracking-[2.4] text-primary">STARBOY</p>
     </div>
   )
 }
--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,16 +1,19 @@
-import { ClipboardPenLine, Home, MessageCircle, Scroll, UsersRound } from "lucide-react"
+import { ArrowLeftRight, ClipboardPenLine, Home, MessageCircle, Scroll, UsersRound } from "lucide-react"
 import NavigationItem from "./NavigationItem"
+import CustomLink from "./CustomLink"
 
 const Sidebar = () => {
   return (
-    <div className="w-[var(--sidebar-width)] fixed top-4 left-4 h-[calc(100vh-32px)] bg-container border border-border rounded-xl backdrop-blur-xs p-2">
+    <div className="w-[var(--sidebar-width)] fixed top-4 left-4 h-[calc(100vh-32px)] bg-container border border-border rounded-xl backdrop-blur-xs p-2 flex flex-col gap-1">
       <div className="w-full flex flex-col gap-1 text-sm">
         <NavigationItem title="홈" href="/" icon={<Home size={20} />} />
-        <NavigationItem title="채팅" href="/chat" icon={<MessageCircle size={20} />} />
         <NavigationItem title="과제" href="/task" icon={<ClipboardPenLine size={20} />} />
         <NavigationItem title="성과 보고" href="/report" icon={<Scroll size={20} />} />
         <NavigationItem title="동아리 관리" href="/club" icon={<UsersRound size={20} />} />
       </div>
+      <div className="flex-1" />
+      <p className="text-xs">현재 동아리: <span className="font-black text-sm">ALT</span></p>
+      <CustomLink href="/choose-club" className="w-full bg-border rounded-lg py-1 flex gap-1 items-center justify-center">동아리 변경 <ArrowLeftRight size={16} /></CustomLink>
     </div>
   )
 }
